24fbac3b42ff93f3bc25f71c940e88c44a138b7f
Royce Matthew Chan <royce@Royces-MacBook-Pro.local>
2018-04-15 21:40:00 +0800

    added helloapp

---
+++ helloapp/howdy/models.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class User(models.Model):
+	email = models.CharField(max_length=30)
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
+	shipping_address = models.CharField(max_length=30)
+
+class Cart(models.Model):
+    cart_code = models.CharField(max_length=30)
+    total_price = models.CharField(max_length=30)
+    paid = models.BooleanField()
+
+
+class Product(models.Model):
+    price = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=300)
+
+
+class Cart_Product(models.Model):
+	cart = models.ForeignKey('Cart', on_delete=models.CASCADE)
+	product = models.ForeignKey('Product', on_delete=models.CASCADE)
